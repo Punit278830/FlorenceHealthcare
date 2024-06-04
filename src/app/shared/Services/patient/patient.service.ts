f92f3721b48009ecec00036fd2402b5d24a5e63e
Rishab29-code <69431540+Rishab29-code@users.noreply.github.com>
2024-06-04 13:43:42 +0530

    validation and modal

--- src/app/shared/Services/patient/patient.service.ts
+++ src/app/shared/Services/patient/patient.service.ts
@@ -1,44 +1,42 @@
 import { Injectable } from '@angular/core';
-import { IpatientInfo} from '../../models/models';
+import { IpatientInfo } from '../../models/models';
 import { ApiHttpService } from '../../apiService/apiHttpService';
-import { Observable } from 'rxjs';
+import { Observable, catchError, throwError } from 'rxjs';
 import { api_Url } from 'src/environment/environment';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PatientService {
-//private staffDto!:IstaffInfo;
-//private readonly apiUrl="https://localhost:44320/api/";
-private readonly apiUrl=api_Url;
-public patientId!:number;
-public patintDataForOPD!:IpatientInfo;
-//public patientId!:number
+  //private staffDto!:IstaffInfo;
+  //private readonly apiUrl="https://localhost:44320/api/";
+  private readonly apiUrl = api_Url;
+  public patientId!: number;
+  public patintDataForOPD!: IpatientInfo;
+  //public patientId!:number
 
-  constructor(private _http:ApiHttpService) { }
-  
-  CreatePatient(patientData:IpatientInfo):Observable<any>
-{
-  return this._http.post(this.apiUrl+"PatientInfoes",patientData)
-  
+  constructor(private _http: ApiHttpService) { }
 
-}
+  CreatePatient(patientData: IpatientInfo): Observable<any> {
+    return this._http.post(this.apiUrl + "PatientInfoes", patientData).pipe(
+      catchError(this.handleError)
+    );
+  }
 
-updatePatientData(id:number,patientData:IpatientInfo){
- return this._http.put(this.apiUrl+"PatientInfoes/"+id,patientData)
-}
+  updatePatientData(id: number, patientData: IpatientInfo) {
+    return this._http.put(this.apiUrl + "PatientInfoes/" + id, patientData)
+  }
 
- getPatientList():Observable<IpatientInfo[]>
-{
- return this._http.get( this.apiUrl+'PatientInfoes')
+  getPatientList(): Observable<IpatientInfo[]> {
+    return this._http.get(this.apiUrl + 'PatientInfoes')
 
-}
+  }
 
-getPatientdateange(sdate:any,edate:any):Observable<IpatientInfo[]>
-{
- return this._http.get( this.apiUrl+'PatientInfoes/regestrationDateRange/'+sdate+'/'+edate)
+  getPatientdateange(sdate: any, edate: any): Observable<IpatientInfo[]> {
+    return this._http.get(this.apiUrl + 'PatientInfoes/regestrationDateRange/' + sdate + '/' + edate)
 
-}
+  }
 
 
 getPatientData(id:number)
@@ -53,6 +51,16 @@ deletePatient(id:number){
   return this._http.delete(this.apiUrl+"PatientInfoes/"+id)
 
 }
+  getPatientData(id: number) {
+    return this._http.get(this.apiUrl + 'PatientInfoes/' + id);
+  }
+  serarchPatient(data: string) {
+    return this._http.get(this.apiUrl + "PatientInfoes/SearchData?data=" + data)
+  }
+
+  private handleError(err: HttpErrorResponse): Observable<never> {
+    return throwError(() => err.error);
+  }
 }
 
 
