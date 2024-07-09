ea3e7909d733731e832492cd86755c859693ad78
Rishab29-code <69431540+Rishab29-code@users.noreply.github.com>
2024-07-10 01:36:04 +0530

    thermal printfix

--- src/app/core/accounts/invoice-view/invoice-view.component.ts
+++ src/app/core/accounts/invoice-view/invoice-view.component.ts
@@ -9,6 +9,81 @@ import { StaffService } from 'src/app/shared/Services/staff/staff.service';
 import { Iappointment, Iinvoice, IpatientInfo, IstaffInfo } from 'src/app/shared/models/models';
 import { routes } from 'src/app/shared/routes/routes';
 
+import { create, SheetsRegistry } from "jss";
+import preset from "jss-preset-default";
+
+const styles = {
+  singleLine: `
+    margin-top: 0.25rem;
+    margin-bottom: 0.25rem;
+    white-space: pre-wrap;
+  `,
+  printAreaContainer: `
+    padding: 8px;
+  `,
+  fontMono: {
+    fontFamily: "monospace"
+  },
+  textCenter: {
+    textAlign: "center"
+  },
+  textRight: {
+    textAlign: "right"
+  },
+  textLeft: {
+    textAlign: "left"
+  },
+  fontBold: {
+    fontWeight: "bold"
+  },
+  grid3Col: {
+    display: "grid",
+    columnGap: "5px",
+    gridTemplateColumns: "1fr auto auto"
+  },
+  gridBorderSolid: `
+    border-bottom: 1px solid;
+  `,
+  gridBorderDashed: `
+    border-bottom: 1px dashed;
+  `,
+  gridBorderDouble: `
+    border-bottom: 3px double;
+  `,
+  gridBorder: `
+    grid-column: 1 / -1;
+    margin: 4px 0;
+  `,
+  nowrap: {
+    overflow: "hidden",
+    textOverflow: "clip",
+    whiteSpace: "nowrap"
+  },
+  wrapper:{
+    display:"flex",
+    justifyContent:"space-between",
+    width:"70mm"
+
+  },
+  colSpan2: {
+    gridColumn: "span 2 / span 2"
+  },
+  maxLine2: {
+    maxHeight: "30px",
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "-webkit-box",
+    "-webkit-line-clamp": 2,
+    "-webkit-box-orient": "vertical"
+  }
+};
+// const sheets = new SheetsRegistry();
+// const sheet = jss.createStyleSheet(styles);
+// sheets.add(sheet);
+// const { classes } = sheet.attach();
+
+
+
 @Component({
   selector: 'app-invoice-view',
   templateUrl: './invoice-view.component.html',
@@ -17,6 +92,7 @@ import { routes } from 'src/app/shared/routes/routes';
 export class InvoiceViewComponent implements OnInit {
   @ViewChild('printview', { static: false }) printView!: ElementRef;
   public routes = routes;
+  private sheets!: SheetsRegistry;
   public invoiceDetails!: Iinvoice;
   private invoiceId!: number;
   public patientDetails!: IpatientInfo;
@@ -28,6 +104,10 @@ export class InvoiceViewComponent implements OnInit {
   public isPaidButtonVisible = true;
   public appointmentList: any[] = [];
   public flag: boolean = false;
+  public classes:any;
+  public width! : string ;
+  public thermalvisible : boolean = false;
+  public disc:number=0;
 
   constructor(private invoiceService: InvoiceService,
     private patientService: PatientService,
@@ -35,6 +115,7 @@ export class InvoiceViewComponent implements OnInit {
     private staffService: StaffService,
     private toastr: ToastrService,
     private route: Router) {
+      console.log("invoiceId",this.invoiceService.invoiceId);
     if (!this.invoiceService.invoiceId) {
       this.route.navigate(['/accounts/invoices'])
     }
@@ -57,11 +138,11 @@ export class InvoiceViewComponent implements OnInit {
         this.balanceAmount = this.balanceAmount + res.amount;
       }
       this.totalInvoiceAmount += res.amount;
-      
+
       this.getPatientDetails(res.patientId);
       this.getAppointDetails(res.appoitmentId);
       this.getAddtionalItems(this.invoiceId)
-      
+
 
 
 
@@ -73,6 +154,13 @@ export class InvoiceViewComponent implements OnInit {
 
   ngOnInit() {
     this.getInvoiceDetails();
+    const jss = create(preset());
+    this.sheets = new SheetsRegistry();
+    const sheet = jss.createStyleSheet(styles);
+    this.sheets.add(sheet);
+    this.classes = sheet.attach().classes;
+    this.width = '80mm';
+    
 
   }
 
@@ -89,7 +177,7 @@ export class InvoiceViewComponent implements OnInit {
       this.appointmentDetails = res;
       console.log("appoint", this.appointmentDetails)
       this.getDoctorDetails();
-      
+
     })
   }
 
@@ -147,10 +235,10 @@ export class InvoiceViewComponent implements OnInit {
     }
 
     if (this.flag) {
-      this.totalInvoiceAmount = 0;
+      // this.totalInvoiceAmount = 0;
+      this.disc=100;
     }
-
-    console.log("Flag:", this.flag);
+  console.log("Flag:", this.flag);
   }
 
 
@@ -171,12 +259,23 @@ export class InvoiceViewComponent implements OnInit {
   }
 
   print() {
-    this.isPaidButtonVisible = false;
-    const printContents = this.printView.nativeElement.innerHTML;
-    const originalContents = document.body.innerHTML;
-    document.body.innerHTML = printContents;
-    window.print();
-    document.body.innerHTML = originalContents;
+    this.thermalvisible = true;
+    
+    setTimeout(() => {
+      if (this.printView) {
+        const tpm = new ThermalPrinterService('80mm');
+        const styles = this.sheets.toString();
+        console.log(this.printView.nativeElement.innerHTML);
+        console.log(styles);
+        tpm.setStyles(styles);
+        tpm.addRawHtml(this.printView.nativeElement.innerHTML);
+        tpm.print();
+
+        this.thermalvisible=false;
+      } else {
+        console.error('printView is not defined');
+      }
+    }, 0);
   }
 
   getAddtionalItems(id: number) {
@@ -212,6 +311,81 @@ export class InvoiceViewComponent implements OnInit {
     })
 
   }
+  
+  
+  moveToEditInvoice(id:number)
+  {
+    this.invoiceService.invoiceId=id;
+    this.route.navigate(['/invoice/edit-invoice'])
 
+  }
+
+}
+
+class ThermalPrinterService {
+  printContent: string = '';
+  cssStyles = ``;
+
+  constructor(private paperWidth: "80mm" | "58mm") { }
+
+  addRawHtml(htmlEl: string) {
+    this.printContent += `\n${htmlEl}`;
+  }
+
+  addLine(text: string) {
+    this.addRawHtml(`<p>${text}</p>`);
+  }
+
+  addLineWithClassName(className: string, text: string) {
+    this.addRawHtml(`<p class="${className}">${text}</p>`);
+  }
+
+  addEmptyLine() {
+    this.addLine(`&nbsp;`);
+  }
+
+  addLineCenter(text: string) {
+    this.addLineWithClassName("text-center", text);
+  }
+  setStyles(cssStyles: string) {
+    this.cssStyles = cssStyles;
+  }
+
+  print() {
+    const printerWindow = window.open(``, `_blank`);
+    if (printerWindow) {
+      printerWindow.document.write(`
+        <!DOCTYPE html>
+        <html>
+        
+        <head>
+          <title>Print</title>
+          <style>
+            html { padding: 0; margin: 0; width: ${this.paperWidth}; }
+            body { margin: 0; padding: 8px; }
+            p { margin-top: 0.25rem; margin-bottom: 0.25rem; white-space: pre-wrap; }
+            ${this.cssStyles}
+          </style>
+          <script>
+            window.onafterprint = event => {
+              window.close();
+            };
+          </script>
+        </head>
+    
+        <body>
+          ${this.printContent}
+        </body>
+        
+        </html>
+      `);
+      printerWindow.document.close();
+      printerWindow.focus();
+      printerWindow.print();
+    } else {
+      console.error("Failed to open the print window. Please check your browser settings and try again.");
+    }
+    // mywindow.close();
+  }
 }
 
